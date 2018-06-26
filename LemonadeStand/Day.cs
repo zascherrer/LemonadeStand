6ea8faa23a8d158c2bdd01ef055ddc261a75f577
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-06-26 08:11:34 -0500

    Created the Day class as well as the SetWeather and SetTemperature methods in the Weather class

---
+++ LemonadeStand/Day.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Day
+    {
+        Weather weather;
+        List<Customer> customers;
+        Random random;
+
+        public Day()
+        {
+            weather = new Weather();
+            random = new Random();
+
+            AddCustomers();
+        }
+
+        private void AddCustomers()
+        {
+            int howManyCustomers = random.Next(75, 126);
+
+            for (int i = 0; i < howManyCustomers; i++)
+            {
+                customers.Add(new Customer());
+            }
+        }
+
+
+    }
+}
--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -10,6 +10,7 @@ namespace LemonadeStand
     {
         private int temperature;
         private Random random;
+        public string weatherEffect;
         public int Temperature
         {
             get
@@ -26,12 +27,45 @@ namespace LemonadeStand
         {
             random = new Random();
 
-            temperature = random.Next(50, 101);
+            SetWeather();
         }
 
         public void SetWeather()
         {
+            int weatherEffectInt = random.Next(1, 7);
 
+            SetTemperature();
+
+            switch (weatherEffectInt)
+            {
+                case 1:
+                    weatherEffect = "Rain!";
+                    break;
+                case 2:
+                    weatherEffect = "Foggy";
+                    break;
+                case 3:
+                    weatherEffect = "Sunny";
+                    break;
+                case 4:
+                    weatherEffect = "Cloudy";
+                    break;
+                case 5:
+                    weatherEffect = "Overcast";
+                    break;
+                case 6:
+                    weatherEffect = "Humid";
+                    break;
+                default:
+                    weatherEffect = "Something went wrong in the Set Weather function";
+                    break;
+            }
+
+        }
+
+        private void SetTemperature()
+        {
+            temperature = random.Next(50, 101);
         }
     }
 }
