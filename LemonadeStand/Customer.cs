fb9e41f20b652b7fe84cf497f3d0304e9f0225ee
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-06-26 09:00:39 -0500

    Fleshed out the customer preference system and created a class for customer feedback.

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -8,20 +8,93 @@ namespace LemonadeStand
 {
     class Customer
     {
-        private double satisfaction;
         private double pricePreferred;
         private int icePreferred;
         private int sugarPreferred;
         private int lemonsPreferred;
+        private int thirst;
+        private Random random;
 
-        public Customer()
+        public Customer(Weather weather)
         {
-            satisfaction = 0.5;
+            random = new Random();
+            
+            thirst = random.Next(0, 51);
+            SetPreferences(weather);
+
+
         }
 
         private void SetPreferences(Weather weather)
         {
 
+            SetLemonPreference(weather);
+            SetSugarPreference(weather);
+            SetIcePreference(weather);
+            SetThirst(weather);
+
+            SetPricePreference(weather);
+
+        }
+
+        private void SetLemonPreference(Weather weather)
+        {
+            lemonsPreferred = random.Next(3, 6);
+        }
+
+        private void SetSugarPreference(Weather weather)
+        {
+            sugarPreferred = random.Next(4, 7);
+        }
+
+        private void SetIcePreference(Weather weather)
+        {
+            int firstTemperatureThreshold = 75;
+            int secondTemperatureThreshold = 90;
+
+            icePreferred = random.Next(3, 6);
+
+            if (weather.Temperature >= firstTemperatureThreshold)
+            {
+                icePreferred += 1;
+
+                if (weather.Temperature >= secondTemperatureThreshold)
+                {
+                    icePreferred += 1;
+                }
+            }
+        }
+
+        private void SetThirst(Weather weather)
+        {
+            switch (weather.weatherEffect)
+            {
+                case "Rain!":
+                    thirst -= 25;
+                    break;
+                case "Foggy":
+                    thirst -= 15;
+                    break;
+                case "Sunny":
+                    thirst += 15;
+                    break;
+                case "Cloudy":
+                    thirst += 0;
+                    break;
+                case "Overcast":
+                    thirst -= 5;
+                    break;
+                case "Humid":
+                    thirst += 25;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetPricePreference(Weather weather)
+        {
+            pricePreferred = ((lemonsPreferred + sugarPreferred + icePreferred) * 2 + thirst / 2) / 100.0;
         }
 
         private void BuyLemonade()
